187a6a12d4ad0214c16416235e27809ffac26924
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2024-09-16 22:55:39 -0300

    Adicionando páginas iniciais

--- admin-template/src/components/template/MenuItem.tsx
+++ admin-template/src/components/template/MenuItem.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 //criar uma interface para definir o que deve ser passado para o componente
 interface MenuItemProps {
     url: string
@@ -8,8 +10,24 @@ interface MenuItemProps {
 //essa interface acima será referenciada no objeto de parâmetro do método
 export default function MenuItem(props: MenuItemProps) {
     return (
-        <li className={``}>
-            {props.icone}
+        <li className={`hover:bg-gray-100`}>
+
+            <Link href={props.url}>
+
+                <a className={`
+                    flex flex-col justify-center items-center
+                    h-20 w-20
+                `}>
+                    {props.icone}
+                    <span className={`
+                        text-xs font-light text-gray-600
+                    `}>
+                        {props.texto}
+                    </span>
+                </a>
+
+            </Link>
+
         </li>
     )
 }
---
+++ admin-template/src/pages/notificacoes.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/template/Layout'
+
+export default function Notificacoes() {
+    return (
+        <Layout titulo="Notificações" subtitulo="Aque você irá gerenciar as suas notificações.">
+            <h3>Conteúdo!!!!</h3>
+        </Layout>
+    )
+}
