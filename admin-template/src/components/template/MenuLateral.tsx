9103f8b0dcc30930109117bc353e83d9205ddf30
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2024-09-17 23:02:51 -0300

    ajustando componente titulo e criando o Logo.

--- admin-template/src/components/template/MenuLateral.tsx
+++ admin-template/src/components/template/MenuLateral.tsx
@@ -1,9 +1,17 @@
 import { IconeAjustes, IconeCasa, IconeSino } from "../icons"; //icone = vai buscar de dentro da pasta icons 
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral() {
     return (
         <aside>
+            <div className={`
+                flex flex-col items-center justify-center
+                bg-gradient-to-r from-indigo-500 to-purple-800
+                h-20 w-20   
+            `}>
+                <Logo></Logo>
+            </div>
             <ul>
                 <MenuItem url="/" texto="Início" icone={IconeCasa}></MenuItem>
                 <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes}></MenuItem>
