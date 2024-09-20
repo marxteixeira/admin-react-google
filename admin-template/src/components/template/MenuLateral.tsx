cab8070aeb6cf937dd2481ac5dd4ad4fa3424430
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2024-09-19 22:30:39 -0300

    iniciando componente de logout

--- admin-template/src/components/template/MenuLateral.tsx
+++ admin-template/src/components/template/MenuLateral.tsx
@@ -1,10 +1,10 @@
-import { IconeAjustes, IconeCasa, IconeSino } from "../icons"; //icone = vai buscar de dentro da pasta icons 
+import { IconeAjustes, IconeCasa, IconeSair, IconeSino } from "../icons"; //icone = vai buscar de dentro da pasta icons 
 import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral() {
     return (
-        <aside>
+        <aside className="flex flex-col">
             <div className={`
                 flex flex-col items-center justify-center
                 bg-gradient-to-r from-indigo-500 to-purple-800
@@ -12,10 +12,14 @@ export default function MenuLateral() {
             `}>
                 <Logo></Logo>
             </div>
-            <ul>
+            <ul className="flex-grow">
                 <MenuItem url="/" texto="Início" icone={IconeCasa}></MenuItem>
                 <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes}></MenuItem>
                 <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino}></MenuItem>
+            </ul>
+            {/* //essa ul será o ícone de sair */}
+            <ul >
+                <MenuItem url="/" texto="Sair" icone={IconeSair}></MenuItem>
             </ul>
         </aside>
     )
